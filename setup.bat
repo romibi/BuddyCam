6b38efb51b871b957d0ddf502b6589ffab668a93
romibi <romibi@bluewin.ch>
2014-10-10 16:38:34 +0200

    Better setup.bat 2

--- setup.bat
+++ setup.bat
@@ -2,12 +2,6 @@
 echo copy .eclipse_clean to eclipse
 xcopy .eclipse_clean eclipse\ /E /D /Y > .tmp
 del .tmp
-attrib -H eclipse
-attrib +H .*
-attrib +H .settings
-attrib +H .gradle
-attrib +H *.sh
-attrib +H gradlew
-attrib -H .gitignore
+attrib +H .eclipse_clean
 echo gradlew.bat setupDecompWorkspace eclipse
 gradlew.bat setupDecompWorkspace eclipse
